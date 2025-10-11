4a3b1cf0a5a91497eb8725143cc8e12fd7d29b52
njuszj <njuszj@qq.com>
2025-10-11 21:54:39 +0800

    fix tag_album migration

--- server/src/schema/migrations/1756318797208-TagAlbumTable.ts
+++ server/src/schema/migrations/1756318797208-TagAlbumTable.ts
@@ -2,9 +2,15 @@ import { Kysely, sql } from 'kysely';
 
 export async function up(db: Kysely<any>): Promise<void> {
   await sql`CREATE TABLE "tag_album" ("albumsId" uuid NOT NULL, "tagsId" uuid NOT NULL);`.execute(db);
-  await sql`ALTER TABLE "tag_album" ADD CONSTRAINT "PK_tag_album_albumsId_tagsId" PRIMARY KEY ("albumsId", "tagsId");`.execute(db);
-  await sql`ALTER TABLE "tag_album" ADD CONSTRAINT "FK_tag_album_albumsId" FOREIGN KEY ("albumsId") REFERENCES "albums" ("id") ON UPDATE CASCADE ON DELETE CASCADE;`.execute(db);
-  await sql`ALTER TABLE "tag_album" ADD CONSTRAINT "FK_tag_album_tagsId" FOREIGN KEY ("tagsId") REFERENCES "tags" ("id") ON UPDATE CASCADE ON DELETE CASCADE;`.execute(db);
+  await sql`ALTER TABLE "tag_album" ADD CONSTRAINT "PK_tag_album_albumsId_tagsId" PRIMARY KEY ("albumsId", "tagsId");`.execute(
+    db,
+  );
+  await sql`ALTER TABLE "tag_album" ADD CONSTRAINT "FK_tag_album_albumsId" FOREIGN KEY ("albumsId") REFERENCES "album" ("id") ON UPDATE CASCADE ON DELETE CASCADE;`.execute(
+    db,
+  );
+  await sql`ALTER TABLE "tag_album" ADD CONSTRAINT "FK_tag_album_tagsId" FOREIGN KEY ("tagsId") REFERENCES "tag" ("id") ON UPDATE CASCADE ON DELETE CASCADE;`.execute(
+    db,
+  );
   await sql`CREATE INDEX "IDX_tag_album_albumsId" ON "tag_album" ("albumsId");`.execute(db);
   await sql`CREATE INDEX "IDX_tag_album_tagsId" ON "tag_album" ("tagsId");`.execute(db);
   await sql`CREATE INDEX "IDX_tag_album_albumsId_tagsId" ON "tag_album" ("albumsId", "tagsId");`.execute(db);
